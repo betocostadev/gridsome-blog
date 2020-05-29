1d6763ee017c6af525b3a2fbb27dabb0d25881ba
betocostadev <robertomarx@gmail.com>
2020-05-29 18:17:27 -0300

    added more info to graphql query and improved layout

--- content/posts/first-post.md
+++ content/posts/first-post.md
@@ -3,6 +3,11 @@ title: "First Post"
 excerpt: "This is the first post of my blog created using Gridsome"
 date: 2020-05-29
 tags: ['Vue.js', 'vue', 'gridsome', 'markdown', 'blog']
+cover_image: './images/road_into_mountains-Bruno-Bergher.jpg'
+cover_image_alt: 'Long road into the mountains'
+photographer: 'Bruno Bergher'
+photographer_site: 'https://unsplash.com/@bbergher?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText'
+image_from: 'https://unsplash.com/s/photos/road?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText'
 ---
 ## My first blog post
 
--- src/pages/Blog.vue
+++ src/pages/Blog.vue
@@ -1,7 +1,17 @@
 <template>
   <Layout>
     <h1>Blog</h1>
-    <article v-for="edge in $static.allPost.edges" :key="edge.node.id">
+    <article style="margin-bottom: 2.5rem;" v-for="edge in $static.allPost.edges" :key="edge.node.id">
+      <!-- <figure>
+        <g-image :src="edge.node.cover_image" :alt="edge.node.cover_image_alt" style="width:100%" />
+        <figcaption>{{ edge.node.cover_image_alt }}</figcaption>
+      </figure> -->
+      <g-image style="width: 100%" :alt="edge.node.cover_image_alt" :src="edge.node.cover_image" />
+      <small class="cover-image-description">
+        {{`${edge.node.cover_image_alt}. Photo by`}}
+        <a :href="edge.node.photographer_site" target="_blank" rel="noopener">{{ edge.node.photographer }}</a>
+         on <a :href="edge.node.image_from" target="_blank" rel="noopener">Unsplash</a>
+      </small>
       <g-link class="post-link" :to="edge.node.path">{{ edge.node.title }}</g-link>
       <p>{{ edge.node.excerpt }}</p>
       <p>Posted on {{ edge.node.date }}. Time to read {{ edge.node.timeToRead }} minutes.</p>
@@ -27,6 +37,11 @@
         excerpt
         title
         content
+        cover_image_alt
+        cover_image (width: 1200, height: 460, blur: 10, quality: 90)
+        photographer
+        photographer_site
+        image_from
       }
     }
   }
